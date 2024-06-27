ed8659f448582485888f768f4d27712fbbfb4d24
zergger <zergger@foxmail.com>
2024-06-27 04:06:00 +0200

    update pkg

--- pkg/hp_diversity/__init__.py
+++ pkg/hp_diversity/__init__.py
@@ -1,2 +1,2 @@
 from .calculations import d_chao, Cq, Uq, Sq, Vq
-from .data_processing import load_data, calculate_alpha_diversity, calculate_beta_diversity
+from .data_processing import load_data, calculate_alpha_diversity, calculate_beta_diversity, calculate_gamma_diversity
